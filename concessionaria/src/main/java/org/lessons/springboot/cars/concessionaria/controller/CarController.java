66c1985d088e6659aa6cd4cfd5c82cfe286150a1
AlbertoMameli <mamelialberto2@gmail.com>
2025-06-25 20:02:24 +0200

    feat: aggiunta show. Da implementare il delete, create e update.... poi inserire una cartella con img e fare in modo di scorrere le img per visualizzare

--- concessionaria/src/main/java/org/lessons/springboot/cars/concessionaria/controller/CarController.java
+++ concessionaria/src/main/java/org/lessons/springboot/cars/concessionaria/controller/CarController.java
@@ -9,20 +9,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/cars")
+@Controller//sei un controller che va a rispondere alle 
+@RequestMapping("/cars")//richieste che partono nell'url
 public class CarController{
     @Autowired
     private CarRepository carRepository;
 
 
-    @GetMapping
-    public String index(Model model){
-        List<Car> cars = carRepository.findAll();
+    @GetMapping//serve per restituirmi la ricerca
+    public String index(Model model){//prendo il moel di spring!
+        List<Car> cars = carRepository.findAll();//lista dei giochi che mi permette di dialogare con il db
         model.addAttribute("cars", cars);
 
         return "cars/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show (@PathVariable(name ="id")Integer id,Model model){
+
+        model.addAttribute("car",carRepository.findById(id).get());
+        return"cars/show";
     }
 }
--- concessionaria/src/main/java/org/lessons/springboot/cars/concessionaria/model/Car.java
+++ concessionaria/src/main/java/org/lessons/springboot/cars/concessionaria/model/Car.java
@@ -6,8 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "cars_db")
@@ -15,19 +18,40 @@ public class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
     private Integer id;
-    @NotBlank
+    @NotBlank(message = "This name must be not blank, empty or null")
     private String name;
-
+    @NotNull(message = "Insert date please")
     private LocalDate dataImmatricolazione;
+    @NotNull(message = "Insert cilindrata please")
     private Integer cilindrata;
-    private Integer cavalli;
-    private String carrozeria;
-    private float km;
-    private String statoVeicolo;
-    private String urlImage;
 
+    @NotNull(message = "Insert cavalli please")
+    private Integer cavalli;
+
+    @NotNull(message = "Insert kilometers please")
+    private Float km; // Modifica float → Float
+
+    @NotBlank(message = "This name must be not blank, empty or null")
+    private String carrozeria;
+
+    @NotBlank(message = "This camp must be not blank, empty or null")
+    private String statoVeicolo;
+    @Lob
+    @NotBlank(message = "This camp must be not blank, empty or null")
+    private String urlImage;
+    @Lob
+    @Size(min = 10, max = 2000, message = "Description must be contain at least 10 caracters and not more than 2000")
+    @NotBlank(message = "This name must be not blank, empty or null")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Integer getId() {
         return this.id;
@@ -101,10 +125,9 @@ public class Car {
         this.urlImage = urlImage;
     }
 
-
     @Override
     public String toString() {
-        
+
         return this.name;
     }
 
